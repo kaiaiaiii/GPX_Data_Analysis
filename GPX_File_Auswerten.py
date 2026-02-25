da94cdf24091f7cc9a2f0d51d2318c460b381595
kaiaiaiii <kaihrig2012@gmail.com>
2026-02-25 22:49:25 +0100

    refactoring

--- GPX_File_Auswerten.py
+++ GPX_File_Auswerten.py
@@ -2,7 +2,6 @@ import re
 import matplotlib.pyplot as plt
 import tkinter as tk
 import math
-#import rasterio
 import cartopy.crs as ccrs
 import cartopy.io.img_tiles as cimgt
 import numpy as np
@@ -13,9 +12,7 @@ from datetime import datetime
 from requests import get
 import pandas as pd
 from pandas import json_normalize
-tk.Tk().withdraw()
-
-#fn = askopenfilename()
+tk.Tk().withdraw() ##??
 
 ## TODO:
 ## Namingconvention, Iteration x,y gleichgross
@@ -25,10 +22,9 @@ tk.Tk().withdraw()
     ## Steigungen -> done
     ## trittfrequenz bei gegebener Uebersetzung -> Tacho wird gebraucht
     ## Errechnete Leistung 
-    ## 3D-Druckbar?
 
 ### Variablen Definitionen ###
-filename = "/home/kai/repos/GPX_Data_Analysis/FileName.gpx" #askopenfilename() ## mal relativer pfadname
+filename = "./FileName.gpx" #askopenfilename() ## mal relativer pfadname
 latitude, longitude, elevation, time, velocity = [], [], [], [], []
 Leistung_Rollwiderstand, Leistung_Luftwiderstand, Leistung_Steigung = [],[],[]
 velocity = []
@@ -41,16 +37,10 @@ pattern_time = r'<time>(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3})'
 
 with open(filename, 'r') as file:
     for line in file:
-        ### Match patterns 
-        match_lon = re.findall(pattern_longitude, line)
-        match_lat = re.findall(pattern_latitude, line)
-        match_ele = re.findall(pattern_elevation, line)
-        match_time = re.findall(pattern_time, line)
-        ### extend to values
-        longitude.extend(match_lon)
-        latitude.extend(match_lat)
-        elevation.extend(match_ele)
-        time.extend(match_time)
+        longitude.extend(re.findall(pattern_longitude, line))
+        latitude.extend(re.findall(pattern_latitude, line))
+        elevation.extend(re.findall(pattern_elevation, line))
+        time.extend(re.findall(pattern_time, line))
 
 
 ele = list(map(float, elevation))
@@ -107,18 +97,13 @@ def distance(lat, lon, ele):
 ####################
 ### AUSWERTUNGEN ###
 ####################
-
 delta_t = np.diff(time_seconds)
 Distance = distance(lat, long, ele)
 velocities = 3.6*(Distance / np.diff(time_seconds))
-#Leistungen = Leistung_Geschaetzt(100, elevation)
+#Leistungen = Leistung_Geschaetzt(100, elevation)  ->> TODO
 median_velo = np.median(velocities)
 average_velo = np.average(velocities)
 
-#print(lat)
-#print(long)
-#print(median_velo)
-#print(average_velo)
 ###############
 ### plotten ###
 ###############
@@ -145,23 +130,6 @@ plt.xlim([0, 60])#plt.legend()
 plt.savefig("Histogram")
 plt.show()
 plt.close()
-''' Abhaengig von umgebung
-ax = plt.axes(projection=cimgt.OSM().crs)
-ax.set_extent([np.min(long), np.max(long), np.min(lat), np.max(lat)])
-ax.add_image(cimgt.OSM(), 3)
-plt.scatter(long, lat, transform=ccrs.PlateCarree())
-
-leider depreceated:
-fig = plt.figure(figsize=(8, 8))
-m = Basemap(projection='lcc', resolution='f', lat_0=np.average(lat), lon_0=np.average(long), width= 1E5, height=1.2E5) # TODO: Better resolution, automatic with and height
-m.shadedrelief()
-m.drawcoastlines(color='gray')
-m.drawcountries(color='gray')
-m.drawstates(color='gray')
-m.scatter(long, lat, latlon=True, c=ele, s=velocities, cmap='viridis', alpha=0.5)
-plt.colorbar(label=r'$Velocity$')
-plt.clim(3, 7)
-'''
 
 ### das velocities lang genug ist, aber etwas pfuschig
 velocities = np.append(velocities, velocities[-1])
@@ -186,78 +154,26 @@ plt.close()
 ##################
 # min, max of lat, long -> done
 # define coordinates around the trail -> rastering in a rectangle, more shapes would be interesting. Also beeing able to use points according to some landmarks like sea etc
-# api request to get elevation -> save in vector
-# points to stl 
-# 3D-print the points
-'''
-def shape(lat, long, size):
-    lat_max = np.max(lat) + size
-    lat_min = np.min(lat) - size
-    lon_max = np.max(long) + size
-    lon_min = np.min(long) - size
-  return lat_max, lat_min, lon_max, lon_min
-
-'''
-
+# api request to get elevation -> save in vector done
+# points to stl -> TODO
+# 3D-print the points -> TODO
 #own implementation, looking for libraries later#
-
-
-def coordinates(lat, lon):
-    lat_max = np.max(lat)
-    lat_min = np.min(lat)
-    lon_max = np.max(lon)
-    lon_min = np.min(lon) 
-    it_lat_max = lat_max * 100000
-    it_lat_min = lat_min * 100000
-    it_lon_max = lon_max * 100000
-    it_lon_min = lon_min * 100000
-    longitudenvektor, latitudenvektor = [], []  
-    for i in range(int(it_lat_min), int(it_lat_max),1000):
-        latitude = i/100000
-        latitudenvektor.append(latitude)
-    for j in range(int(it_lon_min), int(it_lon_max),1000):
-      longitude = j/100000
-      longitudenvektor.append(longitude)
-    return latitudenvektor, longitudenvektor
-
-          
-def getElevationfromAPI(lat, long): ## open elevation, need to look for other apis maybe
-    query = f"https://api.open-elevation.com/api/v1/lookup?locations={lat},{long}"
-    r = get(query, timeout = 20)
-    if r.status_code in (200, 201):
-            elevation = pd.json_normalize(
-                r.json(), 'results'
-            )['elevation'].values[0]
-    return elevation
-
-def ElevationData(lat, lon):
-    print("Elevationdata")
-    latitudenvektor, longitudenvektor = coordinates(lat, lon)
-
-    elevation_data = []
-
-    for latitude in latitudenvektor:
-        for longitude in longitudenvektor:
-            elevation = getElevationfromAPI(latitude, longitude)
-            elevation_data.append(elevation)
-
-    return elevation_data 
-
 
 def get_elevation_from_Api_post(lat, lon):
     print("Elevationdata")
 
-    latitudenvektor, longitudenvektor = coordinates(lat, lon)
+    lat_max = np.max(lat)
+    lat_min = np.min(lat)
+    lon_max = np.max(lon)
+    lon_min = np.min(lon) 
+    longitudenvektor = np.linspace(lon_min, lon_max, 10)
+    latitudenvektor = np.linspace(lat_min, lat_max, 10)
 
     elevation_data = []
-
     lats_and_lons = []
 
     for latitude in latitudenvektor:
         for longitude in longitudenvektor:
-            # elevation = getElevationfromAPI(latitude, longitude)
-            # elevation_data.append(elevation)
-
             lats_and_lons.append((latitude, longitude))
 
     lats_and_lons_list = lats_and_lons
@@ -270,7 +186,7 @@ def get_elevation_from_Api_post(lat, lon):
     payload = {
     "locations":
     lats_and_lons_list
-}
+    }
 
     headers = {'Accept': 'application/json','Content-Type': 'application/json'}
     query = f"https://api.open-elevation.com/api/v1/lookup"
@@ -287,11 +203,11 @@ def get_elevation_from_Api_post(lat, lon):
 
     return elevation_data
 
-Coordinates_to_plot = coordinates(lat, long)
-Elevationdata = ElevationData(lat, long)
+Data_to_plot = get_elevation_from_Api_post(lat, long) ## maybe have lat, long, ele separated
+#Elevationdata = get_elevation_from_Api_post(lat, long)
 
 plt.figure(figsize=(8, 5)) # TODO: Automatic width and height
-plt.scatter(Coordinates_to_plot[0], Coordinates_to_plot[1], c = ElevationData , cmap = 'viridis' )
+plt.scatter(Data_to_plot[0], Data_to_plot[1], c = Data_to_plot[2] , cmap = 'viridis' )
 plt.xlabel("longitude")
 plt.ylabel("latitude")
 plt.title("Track")
@@ -299,3 +215,59 @@ plt.legend()
 plt.savefig("Track")
 plt.show()
 plt.close()
+
+
+'''
+        ### extend to values
+        longitude.extend(match_lon)
+        latitude.extend(match_lat)
+        elevation.extend(match_ele)
+        time.extend(match_time)
+Abhaengig von umgebung
+ax = plt.axes(projection=cimgt.OSM().crs)
+ax.set_extent([np.min(long), np.max(long), np.min(lat), np.max(lat)])
+ax.add_image(cimgt.OSM(), 3)
+plt.scatter(long, lat, transform=ccrs.PlateCarree())
+
+leider depreceated:
+fig = plt.figure(figsize=(8, 8))
+m = Basemap(projection='lcc', resolution='f', lat_0=np.average(lat), lon_0=np.average(long), width= 1E5, height=1.2E5) # TODO: Better resolution, automatic with and height
+m.shadedrelief()
+m.drawcoastlines(color='gray')
+m.drawcountries(color='gray')
+m.drawstates(color='gray')
+m.scatter(long, lat, latlon=True, c=ele, s=velocities, cmap='viridis', alpha=0.5)
+plt.colorbar(label=r'$Velocity$')
+plt.clim(3, 7)
+def shape(lat, long, size):
+    lat_max = np.max(lat) + size
+    lat_min = np.min(lat) - size
+    lon_max = np.max(long) + size
+    lon_min = np.min(long) - size
+  return lat_max, lat_min, lon_max, lon_min
+
+
+
+def ElevationData(lat, lon):
+    print("Elevationdata")
+    latitudenvektor, longitudenvektor = coordinates(lat, lon)
+
+    elevation_data = []
+
+    for latitude in latitudenvektor:
+        for longitude in longitudenvektor:
+            elevation = getElevationfromAPI(latitude, longitude)
+            elevation_data.append(elevation)
+
+    return elevation_data 
+
+def getElevationfromAPI(lat, long): ## open elevation, need to look for other apis maybe
+    query = f"https://api.open-elevation.com/api/v1/lookup?locations={lat},{long}"
+    r = get(query, timeout = 20)
+    if r.status_code in (200, 201):
+            elevation = pd.json_normalize(
+                r.json(), 'results'
+            )['elevation'].values[0]
+    return elevation
+
+'''
